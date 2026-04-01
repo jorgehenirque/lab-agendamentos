e7501d7809e65d2cea605da4f168a0720dc27cd2
Jorge Henrique <jorgehenrique1718j@gmail.com>
2026-03-31 22:22:13 -0300

    fix: apontar api para producao

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://lab-agendamentos-backend.onrender.com'
 })
 
 api.interceptors.request.use((config) => {
